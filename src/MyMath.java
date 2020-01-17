2bea0e3b163a8790e2a129f51fc8f814e184597f
Jeremy Woo <jeremycw@seas.upenn.edu>
2020-01-17 10:51:42 +0900

    Completed getFactors method

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+
 /**
- * Math functions I've writtten for practice.
+ * Math functions I've written for practice.
  * @author Jeremy Woo
  *
  */
 public class MyMath {
 
+	public static void main(String[] args) {
+		System.out.println(getFactors(36));
+	}
+	
+	/**
+	 * returns positive factors of x
+	 * @param x
+	 * @return
+	 */
+	public static ArrayList<Integer> getFactors(int x) {
+		ArrayList<Integer> factors = new ArrayList<>();
+		
+		for(int i = 1; i <= x; i++) {
+			if(x % i == 0) {
+				factors.add(i);
+			}
+		}
+		
+	return factors;
+	}
+	
 }
